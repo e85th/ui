db9f1f167ce231a38d1e07f465e3972365296df3
Amar Mehta <xfthhxk@gmail.com>
2017-06-20 21:24:49 -0700

    remove schema references

--- src/cljs/e85th/ui/firebase.cljs
+++ src/cljs/e85th/ui/firebase.cljs
@@ -1,19 +1,18 @@
-(ns e85th.ui.firebase
-  (:require [schema.core :as s]))
+(ns e85th.ui.firebase)
 
-(s/defn current-user
+(defn current-user
   "on-user is a function that takes in a user object"
   [on-user]
   (-> (js/firebase.auth)
       (.onAuthStateChanged on-user)))
 
-(s/defn get-token
+(defn get-token
   [user on-token]
   (-> user
       .getToken
       (.then on-token)))
 
-(s/defn current-user-info
+(defn current-user-info
   "on-user is invoked nil if user is not logged in via firebase.
    otherwise a map of keys :name, :email, :photo-url, :uid and :token.
    token can be used to authorize user to the app."
