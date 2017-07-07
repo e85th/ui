e252c978fbae29b19523ecf99d59a23de2693591
Amar Mehta <xfthhxk@gmail.com>
2017-07-07 11:37:41 -0700

    handle ordered/map for edn

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject e85th/ui "0.1.29"
+(defproject e85th/ui "0.1.30"
   :description "Frontend UI code."
   :url "https://github.com/e85th/ui"
   :license {:name "Apache License 2.0"
--- src/cljs/e85th/ui/edn_io.cljs
+++ src/cljs/e85th/ui/edn_io.cljs
@@ -2,6 +2,7 @@
   (:require [cljs-time.core :as t]
             [cljs-time.format :as tf]
             [cljs-time.coerce :as coerce]
+            [cljs.reader]
             [e85th.ui.moment :as moment]
             [e85th.ui.time :as time])
   (:import [goog.date DateTime]))
@@ -12,12 +13,17 @@
   (-pr-writer [o writer opts]
     (write-all writer "#datetime \"" (time/ts->str o) "\"")))
 
-(when js/moment
+(cljs.reader/register-tag-parser! "datetime" time/date-time)
+
+(when (exists?  js/moment)
   (extend-protocol IPrintWithWriter
     js/moment
     (-pr-writer [o writer opts]
-      (write-all writer "#datetime \"" (moment/iso-string o) "\""))))
+      (write-all writer "#datetime \"" (moment/iso-string o) "\"")))
 
-(if js/moment
-  (cljs.reader/register-tag-parser! "datetime" moment/coerce)
-  (cljs.reader/register-tag-parser! "datetime" time/date-time))
+  (cljs.reader/register-tag-parser! "datetime" moment/coerce))
+
+;; for graphql ie lacinia returns flatland's ordered/map which is only
+;; defined in Clojure, mostly don't care about the order of fields in code
+;; except for debugging purposes
+(cljs.reader/register-tag-parser! "ordered/map" (partial into {}))
